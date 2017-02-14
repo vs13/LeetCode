75bb9283fbd2c187b75248153766ed2fc67c7dec
Verma <vervipul@amazon.com>
2017-02-14 22:41:31 +0530

    27. Remove Element

---
+++ 27. Remove Element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slowIndex = 0;
+        for(int i =  0; i < nums.size() ; i++) {
+            if(nums[i] != val) {
+                nums[slowIndex++] = nums[i];
+            }
+        }
+        return slowIndex;
+    }
+};
