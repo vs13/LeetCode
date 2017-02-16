8f7daafd945818f1c55b32887b60f332bdf4d033
Verma <vervipul@amazon.com>
2017-02-16 23:19:37 +0530

    219. Contains Duplicate II

---
+++ 219. Contains Duplicate II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        if (nums.size() == 0 ){
+            return false;
+        }
+        map<int,int> m;
+        for(int i = 0 ; i < nums.size() ; i++) {
+            if(m.find(nums[i]) == m.end())
+                m[nums[i]] = i;
+            else {
+                if((i-m[nums[i]])<=k)
+                    return true;
+                else
+                    m[nums[i]] = i;
+            }
+        }
+        return false;
+    }
+};
