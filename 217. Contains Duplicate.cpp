51fb2e2f8306a5376a4f463b96f33ce34d7c1890
Verma <vervipul@amazon.com>
2017-02-16 23:21:50 +0530

    217. Contains Duplicate

---
+++ 217. Contains Duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int> m;
+        for(int i = 0 ; i < nums.size() ; i++ ) {
+            if(m.find(nums[i]) == m.end())
+                m[nums[i]] = 1;
+            else
+                return true;
+        }
+        return false;
+    }
+};
