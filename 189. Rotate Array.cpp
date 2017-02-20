997f34df294159397f5411cddb2ba2828360e652
Verma <vervipul@amazon.com>
2017-02-20 22:02:32 +0530

    189. Rotate Array

---
+++ 189. Rotate Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+        if(k == 0)
+            return ;
+        reverse(nums.begin(), nums.begin()+(n-k));
+        reverse(nums.begin()+(n-k), nums.end());
+        reverse(nums.begin(), nums.end());
+    }
+};
